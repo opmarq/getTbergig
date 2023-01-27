4c88ed111c4b21164293339ef27f80fc8278bd67
omar.chajia <omar.chajia@adevinta.com>
2023-01-27 11:57:56 +0100

    go

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { ApolloProvider } from "@apollo/client";
+import { Analytics } from "@vercel/analytics/react";
 
 import { createApolloClient } from "../utils/apollo-client";
 import { PostsProvider } from "../components/PostsContext";
@@ -9,13 +10,16 @@ const client = createApolloClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <PostsProvider>
-        <ApolloProvider client={client}>
-          <Component {...pageProps} />
-        </ApolloProvider>
-      </PostsProvider>
-    </ChakraProvider>
+    <>
+      <Analytics />
+      <ChakraProvider theme={theme}>
+        <PostsProvider>
+          <ApolloProvider client={client}>
+            <Component {...pageProps} />
+          </ApolloProvider>
+        </PostsProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
